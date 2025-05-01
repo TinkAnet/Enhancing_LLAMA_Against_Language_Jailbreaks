a9b0d4794b4f038693e2dfee78ac7aaa8f05829c
sebastianvne <jinghanwang20@gmail.com>
2025-05-01 18:50:20 +0800

    gemini regenerate

--- src/Over-refusal/gemini_rerun.py
+++ src/Over-refusal/gemini_rerun.py
@@ -15,7 +15,9 @@ def evaluate_sentence(sentence):
     
     headers = {"Authorization": f"Bearer {API_KEY}"}
     resp = requests.post(URL, headers=headers, json=payload, timeout=60)
-    print(resp.json())
+    if resp.json()['choices'][0]['finish_reason'] == 'content_filter: PROHIBITED_CONTENT':
+        print("Prohibited Content")
+        return "Prohibited_content"
     response_content = resp.json()["choices"][0]["message"]["content"]
     return response_content
 
@@ -23,13 +25,14 @@ load_dotenv()
 API_KEY = os.getenv("api_key")
 URL = "https://api.ohmygpt.com/v1/chat/completions"
 
-with open("/Users/wangjinghan/Documents/GitHub/Enhancing_LLAMA_Against_Language_Jailbreaks/results/implicit_judgment/implicit_contrast/QwQ_32b/fraud_implicit.json",'r') as file:
+with open('/Users/wangjinghan/Documents/GitHub/Enhancing_LLAMA_Against_Language_Jailbreaks/results/implicit_judgment/implicit_contrast/gemini/pornography_sexual_minors_implicit.json','w+') as file:
     data = json.load(file)
-    result = evaluate_sentence(data[99]['AttackPrompt'])
-    print(result)
-    data[99]['Output']
-
-with open("/Users/wangjinghan/Documents/GitHub/Enhancing_LLAMA_Against_Language_Jailbreaks/results/implicit_judgment/implicit_contrast/QwQ_32b/race_implicit.json",'r') as file:
-    data = json.load(file)
-    result = evaluate_sentence(data[75]['AttackPrompt'])
-    print(result)
+    for item in data:
+        try:
+            get = item['Output']
+        except:
+            output = evaluate_sentence(item['AttackPrompt'])
+            item['Output'] = output
+            print(f"modify id={item['id']}")
+    print(data)
+    json.dump(data,file,indent=2)
