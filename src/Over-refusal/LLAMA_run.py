44eaccd3e2e0aae09e6a992777c777091e38070d
sebastianvne <jinghanwang20@gmail.com>
2025-05-01 20:15:39 +0800

    add LLAM_run.py to generate llama over-refusal dataset

--- src/Over-refusal/LLAMA_run.py
+++ src/Over-refusal/LLAMA_run.py
@@ -1,2 +1,40 @@
 import ollama
+import os
+import json
+from filter import *
 
+def generate(text):
+    response = ollama.generate(
+        model = "LLAMA3.3-70b",
+        prompt=text,
+        options={'system':""}
+    )
+    content = response['response']
+    return content
+
+def main(root):
+    with open(f"{root}/Over-refusal/sap_sentences.json",'r') as file:
+        sentences = json.load(file)
+        index = 0
+        data = []
+        print("Generating refusal dataset......")
+        for text in sentences:
+            index += 1
+            result = generate(text)
+            item = {
+                "input":text,
+                "system_prompt":"",
+                "output":result,
+                "is_refuse":is_refuse(result),
+                "id":index,
+            }
+            data.append(item)
+            if index % 10 == 0:
+                with open(f"{root}/Over-refusal/implicit-result/raw/LLAMA3.3-70b/implicit_results.json",'w') as storage:
+                    json.dump(data,storage,indent=2)
+        with open(f"{root}/Over-refusal/implicit-result/implicit_results_LLAMA.json",'w') as storage:
+            json.dump(data,storage,indent=2)
+
+if __name__ == '__main__':
+    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+    main(PROJECT_ROOT)
